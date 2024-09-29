0c7da77f1c6966431de67bd31d15fdb5e6633dba
Nicolas Frasson Boaroto <nicfra0710@gmail.com>
2024-09-29 18:57:52 -0300

    Hello World

---
+++ src/main/java/br/com/frasson/ApiRestApplication.java
@@ -0,0 +1,11 @@
+package br.com.frasson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestApplication.class, args);
+    }
+}
